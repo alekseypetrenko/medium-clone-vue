d90c33fa694bbc0faa2cb687886814d38d1da166
alekseypetrenko <apetrenko.job@gmail.com>
2021-02-08 23:19:43 +0200

    top bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <div id="app">
+    <mcv-topbar />
     <router-view></router-view>
   </div>
 </template>
+
+<script>
+import McvTopbar from '@/components/Topbar';
+export default {
+  name: 'McvApp',
+  components: {
+    McvTopbar
+  }
+};
+</script>
