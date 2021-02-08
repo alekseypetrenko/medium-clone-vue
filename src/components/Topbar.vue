48d53f18cea3d76473d57017e08e19923f9a5723
alekseypetrenko <apetrenko.job@gmail.com>
2021-02-09 00:38:39 +0200

    add getters; refactor code, add mapGetters instead of mapState to use .getters

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -40,8 +40,7 @@
             <router-link
               class="nav-link"
               :to="{
-                name: 'Profile',
-                params: {slug: currentUser.username}
+                name: 'Profile'
               }"
               active-class="active"
             >
@@ -55,7 +54,7 @@
             </router-link>
           </li>
         </template>
-        <template v-if="!isLoggedIn">
+        <template v-if="isAnonymous">
           <li class="nav-item">
             <router-link
               class="nav-link"
@@ -81,15 +80,32 @@
 </template>
 
 <script>
-import {mapState} from 'vuex';
+import {mapGetters} from 'vuex';
+import {getterTypes} from '@/store/modules/auth';
 
 export default {
   name: 'McvTopbar',
   computed: {
-    ...mapState({
-      currentUser: state => state.auth.currentUser,
-      isLoggedIn: state => state.auth.isLoggedIn
+    ...mapGetters({
+      currentUser: getterTypes.currentUser,
+      isLoggedIn: getterTypes.isLoggedIn,
+      isAnonymous: getterTypes.isAnonymous
     })
+    // для уменьшения кода можно использовать mapGetters
+    // ...mapState({
+    // currentUser: state => state.auth.currentUser,
+    // isLoggedIn: state => state.auth.isLoggedIn
+    // }),
+
+    // currentUser() {
+    //   return this.$store.getters[getterTypes.currentUser];
+    // },
+    // isLoggedIn() {
+    //   return this.$store.getters[getterTypes.isLoggedIn];
+    // },
+    // isAnonymous() {
+    //   return this.$store.getters[getterTypes.isAnonymous];
+    // }
   }
 };
 </script>
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -23,6 +23,18 @@ export const actionTypes = {
   login: '[auth] login'
 };
 
+export const getterTypes = {
+  currentUser: '[auth] currentUser',
+  isLoggedIn: '[auth] isLoggedIn',
+  isAnonymous: '[auth] isAnonymous'
+};
+
+const getters = {
+  [getterTypes.currentUser]: state => state.currentUser,
+  [getterTypes.isLoggedIn]: state => Boolean(state.isLoggedIn),
+  [getterTypes.isAnonymous]: state => state.isLoggedIn === false
+};
+
 const mutations = {
   [mutationTypes.registerStart](state) {
     state.isSubmitting = true;
@@ -48,6 +60,7 @@ const mutations = {
   },
   [mutationTypes.loginFailure](state, errorsPayload) {
     state.isSubmitting = false;
+    state.isLoggedIn = false;
     state.validationErrors = errorsPayload;
   }
 };
@@ -94,5 +107,6 @@ const actions = {
 export default {
   state,
   mutations,
-  actions
+  actions,
+  getters
 };
